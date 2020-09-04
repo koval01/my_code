3458f4d69dab5cb8366cae4bb76753ba3a8c3fc1
Koval <61350874+koval01@users.noreply.github.com>
2020-09-04 21:10:22 +0300

    select port

--- b0mb3r_cli/cmd_bomb.py
+++ b0mb3r_cli/cmd_bomb.py
@@ -10,20 +10,26 @@ print("This script is written exclusively for b0mb3r. Developer Koval Yaroslav. 
 print("If you are using Linux on a server (VPS / VDS) then you may have problems with Cyrillic. It is necessary to Russify the system.")
 lang = input("Select lang (EN, UA, RU): ")
 
-def spam_start(phone, cycles, lang, time_while):
+def spam_start(phone, cycles, lang, time_while, port):
 	phone = re.sub(r'[^0-9.]+', r'', phone)
 	cycles = re.sub(r'[^0-9.]+', r'', cycles)
 	time_while = re.sub(r'[^0-9.]+', r'', time_while)
+	port = re.sub(r'[^0-9.]+', r'', port)
+	if port == '':
+		port = str('8080')
 	if lang == "EN":
 		print('b0mb3r starting...')
 	elif lang == "UA":
 		print('Запуск b0mb3r...')
 	elif lang == "RU":
 		print('Запуск b0mb3r...')
-	bomb_start = subprocess.Popen("b0mb3r --only-api")
+	if port == '8080' or port == '':
+		bomb_start = subprocess.Popen("b0mb3r --only-api")
+	elif port != '8080':
+		bomb_start = subprocess.Popen("b0mb3r --only-api --port " + str(port))
 	time.sleep(10)
 	try:
-		spam_request = requests.post("http://127.0.0.1:8080/attack/start", json={
+		spam_request = requests.post("http://127.0.0.1:{}/attack/start".format(str(port)), json={
 			"number_of_cycles": int(cycles),
 			"phone": int(phone)
 			})
@@ -32,7 +38,7 @@ def spam_start(phone, cycles, lang, time_while):
 	json_data = json.loads(spam_request.text)
 	resp_status = str(json_data["success"])
 	resp_id = str(json_data["id"])
-	link_check_status = "http://127.0.0.1:8080/attack/" + resp_id + "/status"
+	link_check_status = "http://127.0.0.1:{}/attack/".format(str(port)) + resp_id + "/status"
 	if resp_status:
 		spam_work = True
 		temp_int_stat = 0
@@ -70,28 +76,34 @@ def spam_start(phone, cycles, lang, time_while):
 		exit("b0bm3r cli завершил работу, поэтому был закрыт.")
 
 if lang == "EN":
+	print("What port you want use?")
+	port = input("Work port: ")
 	print("Example of entering a phone number for Ukraine - +380985577999 or 380985577999")
 	phone = input("Phone number: ")
 	print("Do not enter too large a value, 50-100 is recommended")
 	cycles = input("Cycles count: ")
 	print("For example, if you want to run this script on the server at night, it is desirable to put 30 seconds or more.\nIf you run the script on phones or PCs, you can put 2 seconds or for example 0.5")
 	time_while = input("Time while: ")
-	spam_start(phone, cycles, lang, time_while)
+	spam_start(phone, cycles, lang, time_while, port)
 elif lang == "UA":
+	print("Який порт ви хочете використовувати?")
+	port = input("Робочий порт: ")
 	print("Приклад вводу номера телефону для України - +380985577999 або 380985577999")
 	phone = input("Номер телефону: ")
 	print("Не вводьте занад-то велике значення, рекомендовано 50-100")
 	cycles = input("Кількість циклів: ")
 	print("Наприклад, якщо ви хочете запустити цей скрипт на сервері на ніч, то бажано ставити 30 секунд, чи більше.\nЯкщо ви запускаєте скрипт на телефони чи ПК то можете поставити 2 секунди чи наприклад 0.5")
 	time_while = input("Час циклу: ")
-	spam_start(phone, cycles, lang, time_while)
+	spam_start(phone, cycles, lang, time_while, port)
 elif lang == "RU":
+	print("Какой порт вы хотите использовать?")
+	port = input("Рабочий порт: ")
 	print("Пример ввода номера телефона для Украины - +380985577999 или 380985577999")
 	phone = input("Номер телефона: ")
 	print("Не вводите слишком большое значение, рекомендуется 50-100")
 	cycles = input("Количество циклов: ")
 	print("Например, если вы хотите запустить этот скрипт на сервере на ночь, то желательно ставить 30 секунд, или больше.\nЕсли вы запускаете скрипт на телефоны или ПК то можете поставить 2 секунды или, например 0.5")
 	time_while = input("Время цикла: ")
-	spam_start(phone, cycles, lang, time_while)
+	spam_start(phone, cycles, lang, time_while, port)
 else:
 	print("Lang error!")
